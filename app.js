807ed0a80cf3fc9920c0c90e7b35ab20c6afcf5a
giovanni-arduini <gionnibigudd@gmail.com>
2024-10-22 14:36:42 +0200

    ADD addEventListener to button

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 const lampBulb = document.querySelector(".lamp-img");
-
 console.log(lampBulb);
+
+const button = document.querySelector(".btn");
+console.log(button);
+
+button.addEventListener("click", clickBtn);
+
+function clickBtn() {
+  console.log("Ho cliccato");
+}
+// console.log(switchOnOff);
