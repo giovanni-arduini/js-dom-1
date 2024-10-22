1a47d8c1361a22695559afa54a49244003b2a550
giovanni-arduini <gionnibigudd@gmail.com>
2024-10-22 16:58:33 +0200

    FIX minor fixes in structure

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ console.log(lampBulb);
 const button = document.querySelector(".btn");
 console.log(button);
 
+const backGround = document.querySelector(".bg-dark");
+console.log(backGround);
+
 button.addEventListener("click", function () {
   {
     if (lampBulb.className.includes("bulb-off")) {
@@ -11,10 +14,16 @@ button.addEventListener("click", function () {
       // console.log(lampBulb.className);
       lampBulb.src = "./img/yellow_lamp.png";
       lampBulb.classList.toggle("bulb-off");
+      backGround.classList.toggle("bg-dark");
+      // backGround.classList.remove("bg-dark");
+      // background.classList.add("bg-dark-light");
+
       button.textContent = "Spegni";
     } else {
       lampBulb.src = "./img/white_lamp.png";
       lampBulb.classList.toggle("bulb-off");
+      backGround.classList.toggle("bg-dark");
+
       button.textContent = "Accendi";
     }
   }
